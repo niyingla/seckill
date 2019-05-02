b9f6cc1855ab6706d5f60939a691101a184cc620
pikaqiu <1047984367@qq.com>
2019-05-02 21:35:07 +0800

    增加注释

--- src/main/java/com/pikaqiu/miaosha/dao/MiaoshaUserDao.java
+++ src/main/java/com/pikaqiu/miaosha/dao/MiaoshaUserDao.java
@@ -8,10 +8,10 @@ import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MiaoshaUserDao {
-	
-	@Select("select * from miaosha_user where id = #{id}")
-	public MiaoshaUser getById(@Param("id")long id);
 
-	@Update("update miaosha_user set password = #{password} where id = #{id}")
-	public void update(MiaoshaUser toBeUpdate);
+    @Select("select * from miaosha_user where id = #{id}")
+    MiaoshaUser getById(@Param("id") long id);
+
+    @Update("update miaosha_user set password = #{password} where id = #{id}")
+    void update(MiaoshaUser toBeUpdate);
 }
