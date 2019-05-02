1c8fdabe46e30144f8ea1b7f0ac09ac49d9bb8ae
pikaqiu <1047984367@qq.com>
2019-05-02 21:34:38 +0800

    增加注释

--- src/main/java/com/pikaqiu/miaosha/config/UserArgumentResolver.java
+++ src/main/java/com/pikaqiu/miaosha/config/UserArgumentResolver.java
@@ -16,12 +16,14 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
 	@Autowired
     MiaoshaUserService userService;
-	
+
+	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		Class<?> clazz = parameter.getParameterType();
 		return clazz== MiaoshaUser.class;
 	}
 
+	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 		return UserContext.getUser();
