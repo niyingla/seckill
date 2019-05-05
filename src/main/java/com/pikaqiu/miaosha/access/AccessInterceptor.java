51e0f4552ec5898d7381549f56ee324db29e720c
1047984367@qq.com <as1314520>
2019-05-05 09:51:33 +0800

    增加注释

--- src/main/java/com/pikaqiu/miaosha/access/AccessInterceptor.java
+++ src/main/java/com/pikaqiu/miaosha/access/AccessInterceptor.java
@@ -20,13 +20,24 @@ import java.io.OutputStream;
 
 @Service
 public class AccessInterceptor  extends HandlerInterceptorAdapter{
-	
+
+	/**
+	 * controller调用前 调用拦截器
+	 */
 	@Autowired
     MiaoshaUserService userService;
 	
 	@Autowired
 	RedisService redisService;
-	
+
+	/**
+	 * 前置方法判断是否请求超额 （其他用的默认）
+	 * @param request
+	 * @param response
+	 * @param handler
+	 * @return
+	 * @throws Exception
+	 */
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		if(handler instanceof HandlerMethod) {
@@ -43,31 +54,42 @@ public class AccessInterceptor  extends HandlerInterceptorAdapter{
 			int maxCount = accessLimit.maxCount();
 			boolean needLogin = accessLimit.needLogin();
 
-			//
+			//路径为key 可以限制接口访问次数
 			String key = request.getRequestURI();
 			if(needLogin) {
+				//用户未登录 直接返回false
 				if(user == null) {
+					//返回会话错误
 					render(response, CodeMsg.SESSION_ERROR);
 					return false;
 				}
 				key += "_" + user.getId();
-			}else {
-				//do nothing
 			}
+			//获取访问数
 			AccessKey ak = AccessKey.withExpire(seconds);
+
 			Integer count = redisService.get(ak, key, Integer.class);
+			//当前访问数  不存在设置为1
 	    	if(count  == null) {
 	    		 redisService.set(ak, key, 1);
 	    	}else if(count < maxCount) {
+	    		//存在 -》自增
 	    		 redisService.incr(ak, key);
 	    	}else {
+	    		//请求超额
 	    		render(response, CodeMsg.ACCESS_LIMIT_REACHED);
 	    		return false;
 	    	}
 		}
 		return true;
 	}
-	
+
+	/**
+	 * 输出错误结果
+	 * @param response
+	 * @param cm
+	 * @throws Exception
+	 */
 	private void render(HttpServletResponse response, CodeMsg cm)throws Exception {
 		response.setContentType("application/json;charset=UTF-8");
 		OutputStream out = response.getOutputStream();
@@ -77,6 +99,12 @@ public class AccessInterceptor  extends HandlerInterceptorAdapter{
 		out.close();
 	}
 
+	/**
+	 * 通过登陆token 获取用户信息
+	 * @param request
+	 * @param response
+	 * @return
+	 */
 	private MiaoshaUser getUser(HttpServletRequest request, HttpServletResponse response) {
 		String paramToken = request.getParameter(MiaoshaUserService.COOKI_NAME_TOKEN);
 		String cookieToken = getCookieValue(request, MiaoshaUserService.COOKI_NAME_TOKEN);
@@ -86,7 +114,14 @@ public class AccessInterceptor  extends HandlerInterceptorAdapter{
 		String token = StringUtils.isEmpty(paramToken)?cookieToken:paramToken;
 		return userService.getByToken(response, token);
 	}
-	
+
+
+	/**
+	 * 获取需要的cookiName 的cooki值
+	 * @param request
+	 * @param cookiName
+	 * @return
+	 */
 	private String getCookieValue(HttpServletRequest request, String cookiName) {
 		Cookie[]  cookies = request.getCookies();
 		if(cookies == null || cookies.length <= 0){
